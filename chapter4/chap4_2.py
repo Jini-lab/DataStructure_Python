b7e9841233674a12918a4bd0e47e12cea225480f
Chung yeojin <84843910+Jini-lab@users.noreply.github.com>
2024-04-23 12:26:13 +0900

    Update chap4_2.py

--- chapter4/chap4_2.py
+++ chapter4/chap4_2.py
@@ -14,3 +14,6 @@ s2 = Sample()
 s1.numEven(15)
 s2.numEven(15)
 print(s1.counter, s1.counter2)
+
+# numEven 메서드에서 self.counter를 사용하려면 self를 메서드의 첫 번째 매개변수로 가져와야 합니다.
+# 매개변수로 호출해야지만, numEven 메소드에서 self를 통해 해당 인스턴스의 속성에 접근할 수 있습니다. 
